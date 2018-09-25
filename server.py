54980f6196e18d247072ae3dfbaf771e400786ce
anuta razbitneva <anna22375@gmail.com>
2018-09-25 18:48:59 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,42 @@
+import random, os, json, datetime, time
+
+from flask import Flask, Response, render_template
+from pymongo import MongoClient
+from bson import json_util
+
+
+app = Flask(__name__)
+random_numbers = MongoClient('127.0.0.1', 27017).demo.random_numbers
+
+time.sleep(5) # hack for the mongoDb database to get running
+
+@app.route("/add/<int:lower>/<int:upper>")
+def random_generator(lower, upper):
+    number = str(random.randint(lower, upper))
+    random_numbers.update(
+        {"_id" : "lasts"},
+        {"$push" : {
+            "items" : {
+                "$each": [{"value" : number, "date": datetime.datetime.utcnow()}],
+                "$sort" : {"date" : -1},
+                "$slice" : 5
+            }
+        }},
+        upsert=True
+    )
+
+    return Response(number, status=200, mimetype='application/json')
+
+
+@app.route("/")
+def last_number_list():
+    last_numbers = list(random_numbers.find({"_id" : "lasts"}))
+    extracted = [d['value'] for d in last_numbers[0]['items']]
+
+    return render_template('index.html')
+    #return Response(json.dumps(extracted, default=json_util.default), status=200, mimetype='application/json')
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.config['DEBUG'] = True
+    app.run(host='0.0.0.0', port=port)
